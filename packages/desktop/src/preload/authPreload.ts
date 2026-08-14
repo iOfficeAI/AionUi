4509abf2a492413d010e42917e1c8d25628719d8
zhaolinzhi <zhaolinzhi@badousoft.com>
2026-08-14 10:53:48 +0800

    chore(auth): add console.log to preload to verify it runs

--- packages/desktop/src/preload/authPreload.ts
+++ packages/desktop/src/preload/authPreload.ts
@@ -1,5 +1,7 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
+console.log('[authPreload] running, electron =', typeof electron);
+
 contextBridge.exposeInMainWorld('aionuiAuth', {
   postToken: (tokenPayload: unknown) => ipcRenderer.invoke('external-login:post-token', tokenPayload),
 });
